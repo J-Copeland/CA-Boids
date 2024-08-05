5c0f7be78ad30938e163cd2e01cb80df21d1c450
J-Copeland <j.copeland@lancaster.ac.uk>
2024-08-06 00:09:33 +0100

    bug fixed, cursor position adaptive zooming added

--- Assets/Scripts/TriHolder.cs
+++ Assets/Scripts/TriHolder.cs
@@ -6,9 +6,8 @@ using UnityEngine.EventSystems;
 public class TriHolder : MonoBehaviour, IDragHandler, IScrollHandler
 {
 
-    private Vector2 panelCentre = new Vector2(Screen.width * 0.75f, Screen.height / 2); //centre of the triholder panel: 1/2 down screen, 3/4 across from left
+    private Vector3 panelCentre = new Vector2(Screen.width * 0.75f, Screen.height / 2); //centre of the triholder panel: 1/2 down screen, 3/4 across from left
     [SerializeField] private float zoomScaleFactor = 0.05f;
-    [SerializeField] private float zoomPositionScaleFactor = 0.01f;
 
     public void Start()
     {
@@ -17,29 +16,22 @@ public class TriHolder : MonoBehaviour, IDragHandler, IScrollHandler
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("pos: " + transform.position.x + eventData.delta.x);
-        transform.localPosition = new Vector2(Mathf.Clamp(transform.localPosition.x + eventData.delta.x, -400, 400), Mathf.Clamp(transform.localPosition.y + eventData.delta.y, -400, 400));
+        transform.localPosition = new Vector2(Mathf.Clamp(transform.localPosition.x + eventData.delta.x, -400, 400), Mathf.Clamp(transform.localPosition.y + eventData.delta.y, -400, 400)); //FIX: can behave wierdly with zoom-panning
     }
 
     public void OnScroll(PointerEventData eventData)
     {
-        Vector3 newLocalScale = new Vector3(Mathf.Clamp(transform.GetChild(0).localScale.x + eventData.scrollDelta.y * zoomScaleFactor, 0, 5), Mathf.Clamp(transform.GetChild(0).localScale.y + eventData.scrollDelta.y * zoomScaleFactor, 0, 5), 0);
-        float Xdifference = transform.GetChild(0).localScale.x - newLocalScale.x;
-        Debug.Log(transform.GetChild(0).localScale.x + "...   " + newLocalScale.x);
+        Vector3 newLocalScale = new Vector3(Mathf.Clamp(transform.GetChild(0).localScale.x + eventData.scrollDelta.y * zoomScaleFactor, 0.5f, 5), Mathf.Clamp(transform.GetChild(0).localScale.y + eventData.scrollDelta.y * zoomScaleFactor, 0.5f, 5), 0);
+        
         if (transform.GetChild(0).localScale != newLocalScale) //effects inside only trigger on valid scroll
         {
+            float scaleChangeFactor = newLocalScale.x / transform.GetChild(0).localScale.x;
             
             transform.GetChild(0).localScale = newLocalScale;
-            if(eventData.scrollDelta.y > 0)
-            {
-                transform.GetChild(0).position += (new Vector3(Input.mousePosition.x - panelCentre.x, Input.mousePosition.y - panelCentre.y, 0) * (Xdifference)); //subtract difference between mouse position & panelCentre
-            }
-            else
-            {
-                transform.GetChild(0).position += (new Vector3(Input.mousePosition.x - panelCentre.x, Input.mousePosition.y - panelCentre.y, 0) * (Xdifference)); //add ^
-            }
-            Debug.Log(new Vector3(Input.mousePosition.x - panelCentre.x, Input.mousePosition.y - panelCentre.y, 0) * Xdifference + "...   " + Mathf.Abs(Xdifference));
+            transform.GetChild(0).position += new Vector3(Input.mousePosition.x - transform.GetChild(0).position.x, Input.mousePosition.y - transform.GetChild(0).position.y, 0) * (1 - scaleChangeFactor); //diff in position between mouse & obj, mult by relative change in size
+
         }
-        
+
     }
     
 }
